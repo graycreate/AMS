ebbbb10428cc512091a0681b11203c0ee04573eb
ghuiii <hi@ghui.me>
2014-04-03 23:32:54 +0800

    improve the main.xml

--- src/me/ghui/AMS/UI/MainActivity.java
+++ src/me/ghui/AMS/UI/MainActivity.java
@@ -12,7 +12,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class MainActivity extends Activity {
-    TextView tv_teacher_info ;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,26 +21,5 @@ public class MainActivity extends Activity {
 
     private void init() {
 
-        tv_teacher_info = ((TextView) findViewById(R.id.tv_teacher_info));
-    }
-
-    public void onClick_getSelfInfo(View view) {
-        Log.e("ghui", "get teacher Info...");
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                NetUtils.getTeaInfo();
-                tv_teacher_info.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        tv_teacher_info.setText("数据待解析................................................../n "+NetUtils.TEA_INFO_HTML);
-                    }
-                });
-                Log.e("ghui","jjjjjjjjjjj: "+NetUtils.TEA_INFO_HTML);
-                Document document = Jsoup.parse(NetUtils.TEA_INFO_HTML);
-                Log.e("ghui", "数据待解析...");
-                Log.e("ghui", "document:" + document);
-            }
-        }).start();
     }
 }
